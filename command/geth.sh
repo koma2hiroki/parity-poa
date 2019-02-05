016317b1cc7faa3dbb02d436f512fe92f69ccb0e
komatsu.hiroki <>
2019-02-05 14:35:25 +0900

    commit

---
+++ command/geth.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+geth --port 30403 --cache=1024  \
+  --rpc --rpcaddr "0.0.0.0" --rpcport 8450  \
+    --rpcapi "eth,web3,personal,net,db,miner,admin" --rpccorsdomain "*"  \
+  --ws --wsaddr "0.0.0.0" --wsport 8460  \
+    --wsapi "eth,web3,personal,net,db,miner,admin" --wsorigins "*"
