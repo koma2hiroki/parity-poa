cc6db65689dac2be8473e14d29160b2d58fc90f4
komatsu.hiroki <>
2019-02-25 11:41:04 +0900

    commit

--- command/geth-light.sh
+++ command/geth-light.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-geth --syncmode "light" --port 30403 --cache=1024  \
-  --rpc --rpcaddr "0.0.0.0" --rpcport 8450  \
+geth --syncmode "light" --port 30300 --cache=1024  \
+  --rpc --rpcaddr "0.0.0.0" --rpcport 8540  \
     --rpcapi "eth,web3,personal,net,db,miner,admin" --rpccorsdomain "*"  \
-  --ws --wsaddr "0.0.0.0" --wsport 8460  \
+  --ws --wsaddr "0.0.0.0" --wsport 8550  \
     --wsapi "eth,web3,personal,net,db,miner,admin" --wsorigins "*"
